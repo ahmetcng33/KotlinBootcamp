8e23c2eb6423e1a09bcd114c9e3a8fb41f55be97
Ahmet Çıngı <108427186+ahmetcng33@users.noreply.github.com>
2025-04-24 19:23:28 +0300

    24.04.2024 Ödevi Yapıldı
    Bootcamp ilk ödevimi yükledim

---
+++ Odev1.kt
@@ -0,0 +1,64 @@
+package com.example.kotlinweek1.degiskenler
+
+fun main() {
+    val memleket = "Mersin"
+    val ülke = "TR"
+    var telefon = "+90 333 33 33"
+    var postaKodu = 40100
+    var email = "example@gmail.com"
+    val meslek = listOf("Mobil App", "Web Dev", "Aws", "Data Science")
+    var stok = 0
+    var müsteriAdi = listOf("Ahmet", "Osman", "Ayse", "Esra")
+    var bakiye = 0
+    if(bakiye <= 1){
+        println("Fakirsiniz")
+    }
+    var birtday = 2001
+    println(birtday)
+    var maas = 17000+2
+    var medeniDurum = "Bekar"
+    val ürünYorum = listOf("1 yıldız sakın almayın", "5 yıldız süber", "3 yıldız meh")
+    var ödemeTarihi = "2025-04-23"
+    println("Odeme Tarihi: $ödemeTarihi")
+    val ödeme = 0
+    val siparisAdet = 3
+    var arabaModelleri = listOf("Porhsce", "Mercedes", "BMW")
+    var okudugumKitaplar = "Dostoyevski"
+    val indirimMik = -50
+    var odaSayisi = 2+1
+    println(odaSayisi)
+    println(odaSayisi.toString())//2+1 yazar mı denedik tutmadı
+    var enlem = 55
+    var boylam = enlem - 30
+    var ürünAdi = "Biskrem"
+    val yemekFiyat = 180 - indirimMik
+    if(bakiye >= yemekFiyat)
+        println("Siparişiniz alındı")
+    else
+        println("Ac Kaldiniz")
+    val marka = "Versache"
+    var müzikAdi = "Vurgunum"
+    val videoSüresi = 85
+    val ürünPuani = 3.6
+    var resimAdi = "Monalisa"
+    var dosyaFormatı = ".txt"
+    var renkPaleti = listOf("Mor", "Pembe", "Bordo", "Sarı")
+    var renkKodu = listOf("1", "2", "3", "4")
+    val telefonModel = listOf("Apple", "Samsun Galaxy", "Nokia")
+    val ekranBoyutu = 6.3
+    var agirlik = 5
+    var ulusalGün = "23 Nisan"
+    var tatilGünü = ulusalGün
+    var rezervasyonTarih = "2025-8-8"
+    var sokakAdi = "Atatürk Sokagı"
+    var otobüsHatti = "554 Fakulte Talas Express"
+    var kalanDakika = 3
+    val takipKodu = 123984072314
+    var kuponKodu = "Kg var 7.5 korner alt"
+    val faturaAdresi = sokakAdi
+
+
+
+
+
+}
